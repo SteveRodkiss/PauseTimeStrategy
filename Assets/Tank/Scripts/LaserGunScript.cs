762fa87397939f26627ded1ee48d46d2acbd307b
SteveRodkiss <srodkiss@rosmini.school.nz>
2015-07-05 15:54:02 +1200

    Made the tank track and shoot enemies

---
+++ Assets/Tank/Scripts/LaserGunScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserGunScript : MonoBehaviour
+{
+
+	//prefab for laser
+	public GameObject LaserPrefab;
+	bool firing = false;
+	public float LaserCooldown = 0.2f;
+	float LaserDelay = 0f;
+	public AudioClip LaserBlastSound;
+    
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public void FireLaser()
+	{
+		if(!firing)
+		{
+			firing = true;
+			Instantiate(LaserPrefab,transform.position,transform.rotation);
+			GetComponent<AudioSource>().PlayOneShot(LaserBlastSound);
+        }
+		else
+		{
+			LaserDelay += Time.deltaTime;
+			if(LaserDelay >= LaserCooldown)
+			{
+				LaserDelay = 0f;
+				firing = false;
+			}
+		}
+	}
+}
