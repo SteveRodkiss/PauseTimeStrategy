64c4c89ff29df949fcce293c126484de0b7c8c83
SteveRodkiss <srodkiss@rosmini.school.nz>
2015-07-12 17:00:43 +1200

    Added a wheeled tank and a HoverTank to the test2 scene
    i like the hover tank script- its awesome!

---
+++ Assets/AntiGravScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AntiGravScript : MonoBehaviour
+{
+	LayerMask layerMask;
+	public Rigidbody rigidBody;
+	public float HoverDistance = 2f;
+	public float HoverForce = 100f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		layerMask = 1 << LayerMask.NameToLayer("HoverTank");
+		layerMask = ~layerMask;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		//get distance from ground
+		Ray ray = new Ray(transform.position,Vector3.down);
+		RaycastHit hit;
+		if(Physics.Raycast(ray,out hit,HoverDistance * 2f,layerMask))
+		{
+			//we have hit something under us
+			float CurrentHeight = hit.distance;
+
+			float CurrentForce =  HoverForce * (1.0f - (CurrentHeight / HoverDistance));
+
+			rigidBody.AddForceAtPosition(CurrentForce * Vector3.up,transform.position);
+
+
+
+		}
+	}
+}
---
+++ Assets/TankPlayerControl.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankPlayerControl : MonoBehaviour
+{
+	public WheelCollider[] RightWheels;
+	public WheelCollider[] LeftWheels;
+	public float MotorPower = 500f;
+	public float right = 0f;
+	public float left = 0f;
+	//maximum speed for wheels to go at. This is used to simulate mechnical drag
+	public float MaxRPM = 100f;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		right = 0;
+		left = 0;
+
+		if(Input.GetKey(KeyCode.P))
+		{
+			right = MotorPower;
+		}
+		if(Input.GetKey(KeyCode.L))
+		{
+			right = -MotorPower;
+		}
+		if(Input.GetKey(KeyCode.Q))
+		{
+			left = MotorPower;
+		}
+		if(Input.GetKey(KeyCode.A))
+		{
+			left = -MotorPower;
+		}
+
+		foreach(WheelCollider lw in LeftWheels)
+		{
+			if(left != 0)
+			{
+				lw.motorTorque = left - (left * Mathf.Abs(lw.rpm / MaxRPM));
+				lw.brakeTorque = 0f;
+			}
+			else
+			{
+				lw.brakeTorque = MotorPower * 10f;
+				lw.motorTorque = 0f;
+			}
+		}
+
+		foreach(WheelCollider rw in RightWheels)
+		{
+			if(right != 0)
+			{
+				rw.motorTorque = right  - (right * Mathf.Abs(rw.rpm / MaxRPM));
+				rw.brakeTorque = 0f;
+			}
+			else
+			{
+				rw.brakeTorque = MotorPower * 10f;
+				rw.motorTorque = 0f;
+			}
+		}
+
+
+	}
+}
