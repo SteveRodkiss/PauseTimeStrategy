8c513ade8f13c93a943b86a0ed37b92cb0bbeada
SteveRodkiss <srodkiss@rosmini.school.nz>
2015-07-27 21:19:17 +1200

    Damage Script now on enemy cubes
    small change- I am tired.

---
+++ Assets/Damage.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Damage : MonoBehaviour
+{
+	//amount of starting health
+	public float Health = 50f;
+	
+	void TakeDamage(float damage)
+	{
+		Health -= damage;
+	}
+
+	void Update()
+	{
+		if(Health < 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
